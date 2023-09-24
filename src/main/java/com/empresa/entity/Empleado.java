da69ef5e7c07e5995b18f8919a0be7ec4aac1d04
mysticBel <dnxbel@gmail.com>
2023-09-24 08:38:21 -0500

    annotations : Clase Empleado

--- src/main/java/com/empresa/entity/Empleado.java
+++ src/main/java/com/empresa/entity/Empleado.java
@@ -6,7 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,8 +24,14 @@ public class Empleado {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idEmpleado;
 	private String nombres;
+	
+	@Temporal(TemporalType.DATE)
 	private Date fechaNacimiento;
 	private int estado;
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date fechaRegistro;
+	
+	@ManyToOne
+	@JoinColumn(name="idPais") //del mysql
 	private Pais idPais;  // como atributo Pais
 }
