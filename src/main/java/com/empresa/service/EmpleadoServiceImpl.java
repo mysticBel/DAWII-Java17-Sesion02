9b42c97cf1426163b9305f848f7d815dc52e9e8b
mysticBel <dnxbel@gmail.com>
2023-09-24 09:12:18 -0500

    service : EmpleadoService(interface) y EmpleadoServiceImp (class)

---
+++ src/main/java/com/empresa/service/EmpleadoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Empleado;
+
+public class EmpleadoServiceImpl implements EmpleadoService {
+
+	@Override
+	public List<Empleado> listaEmpleado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empleado insertaEmpleado(Empleado obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
