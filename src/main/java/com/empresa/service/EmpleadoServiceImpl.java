59fbf823461562225f3f329183c09dbf0c5bec68
mysticBel <dnxbel@gmail.com>
2023-09-24 09:21:10 -0500

    EmpleadoServiceImpl

--- src/main/java/com/empresa/service/EmpleadoServiceImpl.java
+++ src/main/java/com/empresa/service/EmpleadoServiceImpl.java
@@ -2,20 +2,26 @@ package com.empresa.service;
 
 import java.util.List;
 
-import com.empresa.entity.Empleado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.empresa.entity.Empleado;
+import com.empresa.repository.EmpleadoRepository;
+
+@Service
 public class EmpleadoServiceImpl implements EmpleadoService {
 
+	@Autowired
+	private EmpleadoRepository repositorio;
+	
 	@Override
 	public List<Empleado> listaEmpleado() {
-		// TODO Auto-generated method stub
-		return null;
+		return repositorio.findAll();
 	}
 
 	@Override
 	public Empleado insertaEmpleado(Empleado obj) {
-		// TODO Auto-generated method stub
-		return null;
+		return repositorio.save(obj);
 	}
 
 }
